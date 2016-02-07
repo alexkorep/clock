40344832796f0e817574eccbef3027aa5598d086
Alexey Korepanov <alexey@hidashhi.com>
2016-02-07 21:42:52 +0100

    displaying current time

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,12 +1,39 @@
 var React = require('react')
 
+var style = {
+  fontSize: '50vh',
+  minHeight: '100%',
+  height: '100%',
+  textAlign: 'center',
+}
+
 module.exports = React.createClass({
   displayName: 'ClockApp',
 
+  getInitialState: function() {
+    return {
+      time: '0:00'
+    }
+  },
+
+  componentDidMount: function() {
+    this.updateDate()
+    setInterval(this.updateDate.bind(this), 1000)
+  },
+
+  updateDate: function() {
+    var d = new Date();
+    var h = d.getHours();
+    var m = d.getMinutes();
+    this.setState({
+      time: h + ':' + m
+    })
+  },
+
   render: function () {
     return (
-      <div>
-        CLOCK
+      <div style={style}>
+        {this.state.time}
       </div>
     )
   }
--- www/app.js
+++ www/app.js
@@ -19890,13 +19890,40 @@ ReactDom.render(
 },{"./components/App":"/home/alexey/proj/clock/src/js/components/App.js","fastclick":"/home/alexey/proj/clock/node_modules/fastclick/lib/fastclick.js","react":"/home/alexey/proj/clock/node_modules/react/react.js","react-dom":"/home/alexey/proj/clock/node_modules/react-dom/index.js"}],"/home/alexey/proj/clock/src/js/components/App.js":[function(require,module,exports){
 var React = require('react')
 
+var style = {
+  fontSize: '50vh',
+  minHeight: '100%',
+  height: '100%',
+  textAlign: 'center',
+}
+
 module.exports = React.createClass({
   displayName: 'ClockApp',
 
+  getInitialState: function() {
+    return {
+      time: '0:00'
+    }
+  },
+
+  componentDidMount: function() {
+    this.updateDate()
+    setInterval(this.updateDate.bind(this), 1000)
+  },
+
+  updateDate: function() {
+    var d = new Date();
+    var h = d.getHours();
+    var m = d.getMinutes();
+    this.setState({
+      time: h + ':' + m
+    })
+  },
+
   render: function () {
     return (
-      React.createElement("div", null, 
-        "CLOCK"
+      React.createElement("div", {style: style}, 
+        this.state.time
       )
     )
   }
